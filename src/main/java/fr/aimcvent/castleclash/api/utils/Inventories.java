019f1ca16c812dd5d7ab9623dc30a371c690bd41
Neutron-Pro <neutron@neutronstars.fr>
2024-01-30 15:46:05 +0100

    Mise en place de la gestion des inventaire et de la sidebar

--- src/main/java/fr/aimcvent/castleclash/api/utils/Inventories.java
+++ src/main/java/fr/aimcvent/castleclash/api/utils/Inventories.java
@@ -5,4 +5,9 @@ import org.bukkit.entity.Player;
 
 public interface Inventories {
     void openTeamMenu(Player player, Translation translation);
+    void openSettingsMenu(Player player, Translation translation);
+
+    void openGameSettingsMenu(Player player, Translation translation);
+
+    void openRoleSettingsMenu(Player player, Translation translation);
 }
--- src/main/java/fr/aimcvent/castleclash/api/utils/SidebarKey.java
+++ src/main/java/fr/aimcvent/castleclash/api/utils/SidebarKey.java
@@ -3,4 +3,5 @@ package fr.aimcvent.castleclash.api.utils;
 public class SidebarKey {
     public static final String WHITESPACE  = "whitespace";
     public static final String PLAYERS     = "players";
+    public static final String FOOTER      = "footer";
 }
