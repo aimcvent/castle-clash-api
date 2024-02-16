c4de3ff48cc54f4c4d4ed25beb9fa09ad8d45598
Neutron-Pro <neutron@neutronstars.fr>
2024-02-16 05:51:27 +0100

    Retrait du settings de castle clash pour le chargement des models

--- src/main/java/fr/aimcvent/castleclash/api/role/handler/HandlerModel.java
+++ src/main/java/fr/aimcvent/castleclash/api/role/handler/HandlerModel.java
@@ -2,10 +2,9 @@ package fr.aimcvent.castleclash.api.role.handler;
 
 import fr.aimcvent.castleclash.api.utils.Model;
 import fr.aimcvent.kernel.api.settings.Setting;
-import fr.aimcvent.kernel.api.settings.Settings;
 
 import java.util.Map;
 
 public interface HandlerModel extends Model {
-    Map<String, Setting<?>> loadSettingOf(Settings settings, String identifier);
+    Map<String, Setting<?>> loadSettingOf(String identifier);
 }
