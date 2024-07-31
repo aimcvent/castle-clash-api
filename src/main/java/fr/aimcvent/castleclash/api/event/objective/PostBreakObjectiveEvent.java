68380a3ecf79c219530e48639fea433d74579284
Neutron-Pro <neutron@neutronstars.fr>
2024-07-31 15:49:23 +0200

    Mise en place du calcule de elo

--- src/main/java/fr/aimcvent/castleclash/api/event/objective/PostBreakObjectiveEvent.java
+++ src/main/java/fr/aimcvent/castleclash/api/event/objective/PostBreakObjectiveEvent.java
@@ -1,16 +1,23 @@
 package fr.aimcvent.castleclash.api.event.objective;
 
 import fr.aimcvent.castleclash.api.objective.Objective;
+import fr.aimcvent.castleclash.api.player.Player;
 import fr.aimcvent.kernel.api.event.Event;
 
 public class PostBreakObjectiveEvent implements Event {
     private final Objective objective;
+    private final Player player;
 
-    public PostBreakObjectiveEvent(Objective objective) {
+    public PostBreakObjectiveEvent(Objective objective, Player player) {
         this.objective = objective;
+        this.player = player;
     }
 
     public Objective objective() {
         return this.objective;
+    }
+
+    public Player player() {
+        return this.player;
     }
 }
