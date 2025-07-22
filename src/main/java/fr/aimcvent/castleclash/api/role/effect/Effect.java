a92bc0cfd8c64288814ee6942485baff4ac76e50
Neutron-Pro <neutron@neutronstars.fr>
2025-07-22 17:26:46 +0200

    équilibrage v2

--- src/main/java/fr/aimcvent/castleclash/api/role/effect/Effect.java
+++ src/main/java/fr/aimcvent/castleclash/api/role/effect/Effect.java
@@ -3,6 +3,8 @@ package fr.aimcvent.castleclash.api.role.effect;
 import org.bukkit.potion.PotionEffectType;
 
 public interface Effect {
+    String identifier();
+
     PotionEffectType type();
 
     int level();
--- src/main/java/fr/aimcvent/castleclash/api/role/effect/Effects.java
+++ src/main/java/fr/aimcvent/castleclash/api/role/effect/Effects.java
@@ -8,6 +8,8 @@ public interface Effects extends RoleCollection<Effect> {
 
     boolean has(PotionEffectType type);
 
+    boolean has(String identifier, PotionEffectType type);
+
     int levelOf(PotionEffectType type);
 
     void lock(int second);
